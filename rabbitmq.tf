c0fcbce147faa6d0435e5f04f7edf45d7e201569
eswar <eswar.awsdevops@gmail.com>
2023-01-08 15:31:11 +0530

    creating rabbitmq

--- rabbitmq.tf
+++ rabbitmq.tf
@@ -11,4 +11,3 @@ resource "aws_spot_instance_request" "rabbitmq" {
 labauto ansible
 ansible-pull -i localhost, -U https://github.com/EswarAwsDevOps/roboshop-ansible roboshop.yml -e ROLE_NAME=rabbitmq -e ENV=${var.env}
 EOF
-}
